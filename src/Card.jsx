ce9bc184de6c735032b441b35b25d3ed50711f89
githoboman <smithmar681@gmail.com>
2024-07-17 17:15:48 +0100

    react project commit

---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+
+import profilepic from "./assets/profine.webp"
+
+
+function Card () {
+    return ( 
+        <div className="card">
+            <img className="card-img" src={profilepic} alt="profile picti" />
+            <h2 className="h2x">ayomide olash mi</h2>
+            <p> am  the Goat grow up mehn</p>
+
+        </div>
+
+    )
+ }
+
+ export default Card
