ec5b60a8856d090cb128b96334e9be6692238bd9
githoboman <smithmar681@gmail.com>
2024-07-18 17:27:11 +0100

    guuun men commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
 /* import Card from './Card.jsx' */
 import Button from "./Button.jsx";
+import Student from "./Student.jsx"
 
 function App() {
   return (
     <>
       <Button />
+      <Student name = "Spongebob" age = "30"/>
       {/* <Card></Card>
     <Card></Card>
     <Card></Card> */}
---
+++ src/Student.jsx
@@ -0,0 +1,17 @@
+
+
+function Student (props) {
+    return (
+        <div>
+            <p>
+                Name:{props.name}
+            </p>
+            <p>Age: {props.age} </p>
+            <p>Student: {props.student} </p>
+        </div>
+    );
+
+
+}
+export default Student 
+/* props is a giong to be a javascript object */
