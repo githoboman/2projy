c331b57557e4cc4f61452414437fc932de0addea
githoboman <smithmar681@gmail.com>
2024-07-18 14:27:41 +0100

    button commit

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,14 @@
 /* import styles from './Button.module.css' */
 function Button () {
-    return(<button className=/* {styles.clicky} */ "clicky">click me</button>)
+    const styles = {
+        backgroundColor:" rgba(74, 204, 191, 0.932)",
+        color: "aliceblue",
+        borderRadius: "5px",
+        border: "none",
+        padding:  "10px 20px",
+        cursor:"pointer",
+    }
+    return(<button style={styles}/*  className=/*{styles.clicky} "clicky" */>click me</button>);
 }
 
 export default Button
