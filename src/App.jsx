0db43ec0c85d99a3de8a4be78bc0d25f34cb8558
githoboman <smithmar681@gmail.com>
2024-07-19 16:35:22 +0100

    change commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable no-unused-vars */
 /* import Card from './Card.jsx' */
-import Button from "./Button.jsx";
-import Student from "./Student.jsx"
+import Button from "./components/Button.jsx"
+import Student from "./components/Student.jsx"
 
 function App() {
   return (
     <>
       <Button />
-      <Student name = "Spongebob" age = "30"/>
-      {/* <Card></Card>
-    <Card></Card>
-    <Card></Card> */}
+      <Student name = "Spongebob" age = {30 } Student="true"/>
+      <Student name = 'patrick' age = {42}  Student = "no"></Student>
+      <Student  name = 'squirdward' age = {50}  Student = "no"></Student>
+      <Student name = 'sandy' age = {27 } Student = "true"></Student>
+      <Student/>
+      <Student/>
+      {/* <Card></Card> */}
     </>
   );
 }
---
+++ src/components/Student.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+/* prop types for pass accurate data */
+
+/* default props 
+are default values for props incase they are not passed from the parent component*/
+
+
+
+
+import  PropTypes from 'prop-types'
+
+function Student (props) {
+    return (
+        <div className="studentz">
+            <p>
+                Name :{props.name}
+            </p>
+            <p>Age : {props.age} </p>
+            <p>Student : {props.Student} </p> 
+        </div>
+    );
+
+
+}
+Student.propTypes = {
+    name : PropTypes.string,
+    age :PropTypes.number,
+    Student :PropTypes.string,
+
+}
+
+Student.defaultProps = {
+    name : "guest ",
+    age : [3],
+    Student : "no",
+  }
+export default Student 
+/* props is a giong to be a javascript object */
